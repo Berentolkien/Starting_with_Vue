2862816ce7e6a0e8c9eafd576dfbb7cd02c93598
Berentolkien <bernalb2014@gmail.com>
2023-12-28 16:02:54 -0300

    Clase2:'Option Api', 'CompositionApi'

---
+++ src/components/PostCard.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="post-card">
+        <h3>Titulo</h3>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut nam reprehenderit numquam veritatis sapiente accusamus eius quam exercitationem veniam rerum. Odit dolore distinctio autem at aliquid vero velit accusamus ipsam!</p>
+        <PostButton />
+    </div>
+</template>
+
+<script lang="ts" setup>
+    import PostButton from './PostButton.vue';
+</script>
+
+<style scoped>
+.post-card {
+    border-radius: 15px;
+    padding: 10px;
+    margin: 5px;
+    border: 1px solid #ccc;
+}
+</style>
